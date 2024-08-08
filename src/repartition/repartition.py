6be6bb8dfc44334c593615b7e9e66c5109182986
Mingk42 <xoals123456t@gmail.com>
2024-08-08 12:05:14 +0900

    modified

--- src/repartition/repartition.py
+++ src/repartition/repartition.py
@@ -3,6 +3,6 @@ import pandas as pd
 def repartition(dt="20150101"):
     df = pd.read_parquet(f"/home/root2/data/movie_2015/load_dt={dt}")
 
-    print(df)
-
+    df["load_dt"]=dt
+    
     df.to_parquet("~/data/movie/repartition/", partition_cols=['load_dt','multiMovieYn', 'repNationCd'])
